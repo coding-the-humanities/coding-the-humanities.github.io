d7b3b69e8f5fbaa6bc153d3ecd564bd4083fcf02
Jan Hein Hoogstad <yeehaa123@mac.com>
2014-07-12 00:14:05 +0200

    Updates

---
+++ scripts/post/postCtrl.js
@@ -0,0 +1,8 @@
+(function(){
+  angular.module('cth').controller('PostCtrl', function($scope, $http){
+    $http.get('api/posts.json').success(function(response){
+      console.log(response.posts);
+      $scope.posts = response.posts;
+    });
+  });
+})();
