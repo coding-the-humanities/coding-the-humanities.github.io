29781d01da9b85d36c74568ff36623f60a84b1c1
Jan Hein Hoogstad <yeehaa123@mac.com>
2014-09-16 01:58:18 -0500

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,23 @@
-(function(){
-  'use strict';
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X');ga('send','pageview');
 
-  var app = angular.module('cth', ['ngSanitize', 'templates-cth', 'ngAnimate', 'firebase', 'ui.router']);
+Hyphenator.config({
+  displaytogglebox : false,
+  minwordlength : 8,
+});
 
-  app.constant('Showdown', Showdown);
-  app.constant('$', jQuery);
-  app.constant('_', _);
+Hyphenator.run();
+$(document).ready(function(){
+  $('.snippet .collapse').first().addClass('in');
 
-  app.config(function($stateProvider, $urlRouterProvider) {
+  $('#nav-wrapper').height($("#nav").height());
 
-
-    $stateProvider
-      .state('posts', {
-        url: '/posts',
-        templateUrl: 'post/postList.html',
-        controller: 'PostCtrl as postList',
-      })
-      .state('posts.post', {
-        url: '/:post_id'
-      })
-      .state('about', {
-        url: '/pilot',
-        templateUrl: 'templates/pilot.html',
-        controller: 'PilotCtrl as pilot'
-
-      });
-
-    $urlRouterProvider.when('', '/posts');
+  $('#nav').affix({
+    offset: { top: $('#nav').offset().top }
   });
+});
 
-  app.factory('studentsData', ['$firebase', studentsData]);
-
-  function studentsData($firebase){
-    var ref = new Firebase("https://cth-pilot.firebaseio.com/students");
-    var students = $firebase(ref);
-    return students;
-  }
-
-})();
